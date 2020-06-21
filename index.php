ad6b4b50672c4fb3b332107383ce50dc8d521c44
Ziliesar <heberzp3@gmail.com>
2020-06-20 21:32:06 -0600

    subir pagina web

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Digital Palace</title>
+    <link rel="stylesheet" href="estilosp.css">
+     <link rel="shortcut icon" href="/icono.ico" />
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,700,800" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="logo2.PNG" alt="">
+            </div>
+            <div class="icono" id="icono">
+                <span>&#9776;</span>
+            </div>
+            <div class="enlaces uno" id="enlaces">
+                <a href="#" class="active">Inicio</a>
+                <a href="#">Acerca de nosotros</a>
+                <a href="#">Contacto</a>
+            </div>
+        </nav>
+        <div class="textos">
+            <h1 style="font-size:300%">Las herramientas para triunfar en el Mundo digital</h1>
+            <h2 style="font-size:300%; margin-top:1%">al alcance de tus manos</h2>
+        </div>
+    </header>
+
+    <script src="main.js"></script>
+</body>
+
+</html>
