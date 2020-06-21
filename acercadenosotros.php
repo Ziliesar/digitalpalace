3f39656fbf39f4293128dd16f57fa54dc57fd6e2
Ziliesar <heberzp3@gmail.com>
2020-06-20 22:57:31 -0600

    Agregar Carpeta y archivo acercade

---
+++ acercadenosotros.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Digital Palace</title>
+    <link rel="stylesheet" href="estilosp.css">
+     <link rel="shortcut icon" href="/icono.ico" />
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,700,800" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="logo2.PNG" alt="">
+            </div>
+            <div class="icono" id="icono">
+                <span>&#9776;</span>
+            </div>
+            <div class="enlaces uno" id="enlaces">
+                <a href="#" class="active">Inicio</a>
+                <a href="#">Acerca de nosotros</a>
+                <a href="#">Contacto</a>
+            </div>
+        </nav>
+        
+
+                <IMG SRC="imagenes/acercade/mision.JPG">
+       
+            
+                
+          <IMG SRC="imagenes/acercade/vision.JPG">
+         
+     <IMG SRC="imagenes/acercade/politica.JPG">
+ 
+           
+        <IMG SRC="imagenes/acercade/caracteristicas.jpg">
+    <IMG SRC="imagenes/acercade/seguridad.jpg">
+                 
+        
+        
+    </header>
+
+    <script src="main.js"></script>
+</body>
+
+</html>
