51ed835d97366478ca82e46dfaa782388d7a914f
Ziliesar <heberzp3@gmail.com>
2020-06-21 09:56:18 -0600

    modificar la pagina como plantilla PHP

--- index.php
+++ index.php
@@ -1,50 +1,16 @@
 <!DOCTYPE html>
 <html lang="es">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Digital Palace</title>
-
-     <!--JQury-->
-     <script src="js/jquery-3.5.1.min.js" type="text/javascript"></script>
-    
-    <!-- bootstrap css-->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-    <!-- bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-   
-    <link rel="stylesheet" href="estilos/estilosp2.css">
-     <link rel="shortcut icon" href="img/icono.ico" />
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,700,800" rel="stylesheet">
-</head>
+<?php include("head.php"); ?>
 
 <body>
-    <header>
-        <nav>
-            <div class="logo">
-            <img src="img/logo2.PNG" alt="">
-            </div>
-            <div class="icono" id="icono">
-                <span>&#9776;</span>
-            </div>
-            <div class="enlaces uno" id="enlaces">
-                <a href="#" class="active">Inicio</a>
-                <a href="#">Acerca de nosotros</a>
-                <a href="#">Contacto</a>
-            </div>
-        </nav>
-        <div class="jumbotron textos">
-            <h1 style="font-size:300%">Las herramientas para triunfar en el mundo digital</h1>
-            <h2 style="font-size:300%; margin-top:1%">al alcance de tus manos</h2>
-        </div>
+    <?php include("header.php"); ?>
 
-        
-    </header>
+    <div class="jumbotron textos">
+        <h1 style="font-size:300%">Las herramientas para triunfar en el mundo digital</h1>
+        <h2 style="font-size:300%; margin-top:1%">al alcance de tus manos</h2>
+    </div>
+    
     <script src="js/main.js"></script>
 </body>
 
