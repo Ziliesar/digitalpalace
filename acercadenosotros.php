ba0061198b7078f22b41efe8ad4ca666eec76c05
Ziliesar <heberzp3@gmail.com>
2020-06-21 00:11:33 -0600

    agregar mision

--- acercadenosotros.php
+++ acercadenosotros.php
@@ -6,8 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Digital Palace</title>
-    <link rel="stylesheet" href="estilosp.css">
-     <link rel="shortcut icon" href="/icono.ico" />
+
+    <!--JQury-->
+    <script src="js/jquery-3.5.1.min.js" type="text/javascript"></script>
+    
+    <!-- bootstrap css-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <!-- bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+   
+    <link rel="stylesheet" href="estilos/estilosp2.css">
+     <link rel="shortcut icon" href="img/icono.ico" />
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,700,800" rel="stylesheet">
 
 </head>
@@ -17,7 +29,7 @@
     <header>
         <nav>
             <div class="logo">
-                <img src="logo2.PNG" alt="">
+                <img src="img/logo2.PNG" alt="">
             </div>
             <div class="icono" id="icono">
                 <span>&#9776;</span>
@@ -27,24 +39,22 @@
                 <a href="#">Acerca de nosotros</a>
                 <a href="#">Contacto</a>
             </div>
-        </nav>
-        
-
-                <IMG SRC="imagenes/acercade/mision.JPG">
-       
-            
-                
-          <IMG SRC="imagenes/acercade/vision.JPG">
-         
-     <IMG SRC="imagenes/acercade/politica.JPG">
- 
-           
-        <IMG SRC="imagenes/acercade/caracteristicas.jpg">
-    <IMG SRC="imagenes/acercade/seguridad.jpg">
-                 
-        
-        
+        </nav>  
     </header>
+
+    <br>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="acercadenosotros/mision2.png" style="width: 100%; ">
+            </div>
+
+            <div class="col-md-6">
+                <p class="centrar" style="text-align: justify; margin-top: 80px;">Brindar excelencia y calidad en cada uno de nuestros servicios prestados, proporcionando las mejores herramientas tecnológicas a medida de las necesidades de nuestros clientes, con el objetivo de incrementar su productividad y competitividad en el mundo virtual. Para ello implementamos soluciones prácticas  y creativas, fundamentadas en el avance del desarrollo web, como empresa innovadora buscamos el máximo beneficio para nuestros clientes a través de la calidad de nuestros servicios y productos, creemos en el liderazgo empresarial responsable y en el desarrollo de un crecimiento rentable en conjunto con la sostenibilidad virtualmente progresiva.</p>
+            </div>
+        </div>
+    </div>
 
     <script src="main.js"></script>
 </body>
--- index.php
+++ index.php
@@ -38,10 +38,12 @@
                 <a href="#">Contacto</a>
             </div>
         </nav>
-        <div class="textos">
+        <div class="jumbotron textos">
             <h1 style="font-size:300%">Las herramientas para triunfar en el mundo digital</h1>
             <h2 style="font-size:300%; margin-top:1%">al alcance de tus manos</h2>
         </div>
+
+        
     </header>
     <script src="js/main.js"></script>
 </body>
