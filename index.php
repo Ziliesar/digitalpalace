07f65ae2c0000ea0e9fd6d5a33d93eb198272e8e
Espacio <heberzp3@gmail.com>
2020-06-22 23:18:46 -0600

    ARreglo pagina index

--- index.php
+++ index.php
@@ -5,10 +5,63 @@
 
 <body>
     <?php include("header.php"); ?>
-    <div class="contenedor" id="contobj1">
-        <img class="key" src="img/key2.jpg" />
-        <div class="texto1"></div>
-        <div class="texto2">Las herramientas para triunfar en el mundo digital al alcance de tus manos</div>
+    <div id="contobj1" >
+            <div id="parte1" >
+            <div class="contenedor" >
+                <img class="key" src="img/key2.jpg" />
+                <div class="texto1"></div>
+                <div class="texto2">Las herramientas para triunfar en el mundo digital al alcance 
+                    de tus manos</div>
+            </div>
+            <div id="contenedor2">
+                <div id="texto3" >Desarrollo y mantenimiento de software</div>
+                <div id="texto4">Sabemos de la importancia de sus proyectos en el nuevo mundo digital, el ser entregagos
+                a tiempo y con una calidad superior son factores que nos respaldan como empresa. El trabajo que realizamos
+                con nuestros clientes los ayuda para mantenerse a la vanguardia en la nueva era digital ademas de 
+                ayudar de manera proactiva sus negocios y actividades para satisfarse y susperar sus expectativas.
+                </div>
+            </div>
+                </div>
+
+
+        <div class="container_fluid">
+            <div class="row" style="width:100%;  ">
+                <div class="col-md-6"> 
+                    <img id="imgres" src="img/responsive1.png">
+                </div>
+
+                <div class="col-md-6">
+                    <p class="centrar" id="centrar2">
+                        Brindar excelencia y calidad en cada uno de nuestros servicios prestados, proporcionando las 
+                        mejores herramientas tecnológicas a medida de las necesidades de nuestros clientes, con el objetivo
+                        de incrementar su productividad y competitividad en el mundo virtual. Para ello implementamos 
+                        soluciones prácticas  y creativas, fundamentadas en el avance del desarrollo web, como empresa 
+                        innovadora buscamos el máximo beneficio para nuestros clientes a través de la calidad de nuestros 
+                        servicios y productos, creemos en el liderazgo empresarial responsable y en el desarrollo de un
+                        crecimiento rentable en conjunto con la sostenibilidad virtualmente progresiva.</p>
+                </div>
+            </div>            
+        </div>
+
+        <div class="container_fluid2">
+            <div class="row2" style="width:100%; fond-color:#fff; ">
+                <div class="col-md-62" id="titulof4" > 
+                <FONT size="6%" COLOR="white">LO QUE TÚ TE MERECES</FONT>
+                </div>
+
+                <div class="col-md-62" id="parrafof2">
+                <FONT COLOR="white">Ubicados en Honduras contamos con una gran variedad de 
+                    facilidades para tu negocio o trabajo, ya que realizamos cualquier tipo de proyecto
+                    como ser:<br>
+                    -Elaboracion y mantenimientos de sitios web.<br>
+                    -Elaboracion de apps y programas.
+
+                </FONT>
+                </div>
+            </div>            
+        </div>
+
+
     </div>
     <script src="js/main.js"></script>
 </body>
