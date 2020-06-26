fc93ce3ea9e5945bd7e31577b619c91b5930e6e6
Ziliesar <heberzp3@gmail.com>
2020-06-26 12:21:10 -0600

    modificacion de acercadenosotros

--- acercadenosotros.php
+++ acercadenosotros.php
@@ -8,25 +8,28 @@ include("head.php"); ?>
     <?php include("header.php"); ?>
     
     <div class=" contenAbout">
+        <br>
+        <h1 class="QNS">¿Quienes Somos?</h1>
         <div class="contenAbout1 " >
             <div>
-                <img src="acercadenosotros/mision2.jpg" width="80%" height="40%">
+                <img src="acercadenosotros/mision2.jpg" width="80%" height="40%" style="padding-bottom: 3%;">
+                <br>
                 <p class="font-center" >Brindar excelencia y calidad en cada uno de nuestros servicios prestados, proporcionando las mejores herramientas tecnológicas a medida de las necesidades de nuestros clientes, con el objetivo de incrementar su productividad y competitividad en el mundo virtual.</p>
                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalMision">
                     Leer mas
                 </button>
-
+                
                 <div class="modal fade" id="modalMision" tabindex="-1" role="dialog" aria-labelledby="modalMision" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="modalMision">Mision</h5>
+                            <h5 class="modal-title" id="modalMision" style="color: black;">Mision</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div class="modal-body">
-                            <p class="font-center">
+                            <p class="font-center" style="color: black;">
                             Brindar excelo de nuestros servicios prestados, proporcionando las mejores herramientas tecnológicas a medida de las necesidades de nuestros clientes, con el objetivo de incrementar su productividad y competitividad en el mundo virtual. Para ello implementamos soluciones prácticas  y creativas, fundamentadas en el avance del desarrollo web, como empresa innovadora buscamos el máximo beneficio para nuestros clientes a través de la calidad de nuestros servicios y productos, creemos en el liderazgo empresarial responsable y en el desarrollo de un crecimiento rentable en conjunto con la sostenibilidad virtualmente progresiva.
                             </p>
                         </div>
@@ -41,24 +44,25 @@ include("head.php"); ?>
             </div>
         </div>
         <div class="contenAbout2">
-            <img src="acercadenosotros/vision.jpg" width="80%" height="40%">
+            <img src="acercadenosotros/vision.jpg" width="80%" height="40%" style="padding-bottom: 3%;">
+            <br>
             <p class="font-center">Estar altamente comprometidos con los trabajos realizados para nuestros clientes de manera calificada y eficaz, para convertirnos en su empresa de confianza, logrando identificarnos como una empresa ejemplar...</p>
 
             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalvision">
                     Leer mas
                 </button>
-
+                
                 <div class="modal fade" id="modalvision" tabindex="-1" role="dialog" aria-labelledby="modalvision" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="modalvision">Vision</h5>
+                            <h5 class="modal-title" id="modalvision" style="color: black;">Vision</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div class="modal-body">
-                            <p class="font-center">
+                            <p class="font-center" style="color: black;">
                             Estar altamente comprometidos con los trabajos realizados para nuestros clientes de manera calificada y eficaz, para convertirnos en su empresa de confianza, logrando identificarnos como una empresa ejemplar, que se involucra directamente con el cambio de la tecnología y la sociedad, dando a conocer las posibilidades de acceso a los estándares y herramientas tecnológicas, laborando de forma ética y garantizada. Nos comprometemos a que los productos y servicios sean seguros y estén disponibles para quién lo necesite, la meta que impulsa nuestra empresa, determina nuestro desempeño y propósito en el mundo exterior unificándolo con el mundo virtual de una forma completamente moderna.
                             </p>
                         </div>
@@ -76,23 +80,24 @@ include("head.php"); ?>
     <div class=" contenAbout">
         <div class="contenAbout1">
             <div>
-                <img src="acercadenosotros/politica2.jpg" width="80%" height="40%">
+                <img src="acercadenosotros/politica2.jpg" width="80%" height="40%" style="padding-bottom: 3%;">
+                <br>
                 <p class="font-center">Basada en nuestra firme responsabilidad con los clientes, nos encargamos de satisfacer plenamente sus requerimientos y expectativas...</p>
                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#PoliticaC">
                     Leer mas
                 </button>
-
+                
                 <div class="modal fade" id="PoliticaC" tabindex="-1" role="dialog" aria-labelledby="PoliticaC" aria-hidden="true">
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="PoliticaC">Politica de Seguridad</h5>
+                            <h5 class="modal-title" id="PoliticaC" style="color: black;">Politica de Seguridad</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div class="modal-body">
-                            <p class="font-center">
+                            <p class="font-center" style="color: black;">
                                 Basada en nuestra firme responsabilidad con los clientes, nos encargamos de satisfacer plenamente sus requerimientos y expectativas, por lo que impulsamos una cultura de calidad empresarial establecida en los principios de transparencia, liderazgo y desarrollo de nuestros servicios, comprometiéndonos a la mejora constante y la seguridad en cada uno de nuestros productos brindados.
                             </p>
                         </div>
@@ -107,9 +112,10 @@ include("head.php"); ?>
             </div>
         </div>
         <div class="contenAbout2">
-            <img src="acercadenosotros/caracteristicas.jpg" width="80%" height="40%">
+            <img src="acercadenosotros/caracteristicas.jpg" width="80%" height="40%" style="padding-bottom: 3%;">
+            <br>
             <p class="font-center"></p>
-
+            
             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#CaracterísticasV">
                     Leer mas
                 </button>
@@ -118,18 +124,18 @@ include("head.php"); ?>
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="CaracterísticasV">Características y valores</h5>
+                            <h5 class="modal-title" id="CaracterísticasV" style="color: black;">Características y valores</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div class="modal-body">
                             
-                                <p class="font-center"><b>Integridad:</b> Reflejamos la honestidad y coherencia en todas nuestras acciones como empresa.</p>
-                                <p class="font-center"><b>Responsabilidad:</b> Asumimos compromisos y cumplimos.</p>
-                                <p class="font-center"><b>Calidad:</b> Presentamos un esfuerzo constante por mejorar cada vez más nuestros servicios como empresa, orientados al empeño de realizar bien cada acción desde la primera vez.</p>
-                                <p class="font-center"><b>Respeto:</b> Brindamos respeto y cordialidad con cada uno de los clientes.</p>
-                                <p class="font-center"><b>Vocación de Servicio:</b> Nos adaptamos, diseñamos e ideamos oportunamente, según las expectativas de nuestros clientes cada servicio y producto requerido.</p>
+                                <p class="font-center" style="color: black;"><b>Integridad:</b> Reflejamos la honestidad y coherencia en todas nuestras acciones como empresa.</p>
+                                <p class="font-center" style="color: black;"><b>Responsabilidad:</b> Asumimos compromisos y cumplimos.</p>
+                                <p class="font-center" style="color: black;"><b>Calidad:</b> Presentamos un esfuerzo constante por mejorar cada vez más nuestros servicios como empresa, orientados al empeño de realizar bien cada acción desde la primera vez.</p>
+                                <p class="font-center" style="color: black;"><b>Respeto:</b> Brindamos respeto y cordialidad con cada uno de los clientes.</p>
+                                <p class="font-center" style="color: black;"><b>Vocación de Servicio:</b> Nos adaptamos, diseñamos e ideamos oportunamente, según las expectativas de nuestros clientes cada servicio y producto requerido.</p>
                             
                         </div>
                         <div class="modal-footer">
