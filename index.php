900c1d7ef69cdfe98bc8905295e5ddd5afa2432e
Ziliesar <heberzp3@gmail.com>
2020-06-25 21:24:52 -0600

    agregar cambios index

--- index.php
+++ index.php
@@ -18,21 +18,21 @@
         <div class="carousel-inner">
             <div class="carousel-item active" style="text-align: center;">
                 <img src="img/1c.JPG" alt="1" style="width:80%; text-align: center;">
-                <div class="carousel-caption d-none d-md-block" style="color: white; background-color: black; opacity:80%;">
-                        <h4>Creación de todo tipo de software para todos los dispositivos</h5>
-                        <h5>Apps, juegos y todo tipo de software</p>
-                        </div>
+                <div class="carousel-caption d-block d-md-block" style="color: white; background-color: black; opacity:80%;">
+                    <h4>Creación de todo tipo de software para todos los dispositivos</h5>
+                    <h5>Apps, juegos y todo tipo de software</p>
+                </div>
             </div>
             <div class="carousel-item" >
                 <img src="img/2c.jpg" alt="2" style="width:80%">
-                <div class="carousel-caption d-none d-md-block" style="color: white; background-color: black; opacity:80%;">
+                <div class="carousel-caption d-block d-md-block" style="color: white; background-color: black; opacity:80%;">
                     <h4>Creación y mantenimiento de sitios web</h5>
                     <h5>Elaboramos y le damos mantenimiento a tu sitio web, cumpliendo con todas tus necesidades y expectativas</p>
                     </div>
             </div>
             <div class="carousel-item">
                 <img src="img/3c.JPG" alt="3" style="width:80%">
-                <div class="carousel-caption d-none d-md-block" style="color: white; background-color: black; opacity:80%;">
+                <div class="carousel-caption d-block d-md-block" style="color: white; background-color: black; opacity:80%;">
                     <h4>Ayuda en la creación de software en tus tareas y proyectos universitarios</h5>
                     <h5>¿Eres estudiante informatico o cursas alguna clase de computación?, ponte en contacto con nosotros para recibir ayuda</p>
                     </div>
@@ -40,41 +40,42 @@
         </div>
         
         <!-- flechas de izquierda y derecha -->
-        <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+        <a class="carousel-control-prev" href="#myCarousel" style="background: black; width:12.4%; height: 87.5%; margin-top: 3%;" data-slide="prev">
             <span class="carousel-control-prev-icon"></span>
         </a>
-        <a class="carousel-control-next" href="#myCarousel" data-slide="next">
+        <a class="carousel-control-next" href="#myCarousel" style="background: black; width:12.4%; height: 87.5%; margin-top: 3%;" data-slide="next">
             <span class="carousel-control-next-icon"></span>
         </a>
     </div>
 
-
     <!--CARRUSEL 1-->
 
     <div class="font-center row" id="parrafo2b" style="margin-top:-3%;margin-bottom:-3%">
-                    <h1 class="col-md-12" id="parrafo2">Desarrollo y mantenimiento de software</h1>
-                    <p class="col-md-12" id="parrafo2a">Sabemos de la importancia de sus proyectos en el nuevo mundo digital, el ser entregados
-                    a tiempo y con una calidad superior son factores que nos respaldan como empresa. El trabajo que realizamos
-                    con nuestros clientes los ayuda para mantenerse a la vanguardia en la nueva era digital ademas de 
-                    ayudar de manera proactiva sus negocios y actividades para satisfarse y susperar sus expectativas.
-                    </p>
-                </div>
-            <div id="parte1">
+        <h1 class="col-md-12" id="parrafo2">Desarrollo y mantenimiento de software</h1>
+        <p class="col-md-12" id="parrafo2a">Sabemos de la importancia de sus proyectos en el nuevo mundo digital, el ser entregados
+            a tiempo y con una calidad superior son factores que nos respaldan como empresa. El trabajo que realizamos
+            con nuestros clientes los ayuda para mantenerse a la vanguardia en la nueva era digital ademas de 
+            ayudar de manera proactiva sus negocios y actividades para satisfarse y susperar sus expectativas.
+        </p>
+    </div>
+
+
+        <div id="parte1">
             <!--IMAGEN 1 SLIDER-->
             
-        <!--IMAGEN 1-->
-        <div id="imgp1">
-         <div id="myCarousel2" class="carousel slide" data-ride="carousel">
-            <!-- Indicador -->
-            <ul class="carousel-indicators">
+            <!--IMAGEN 1-->
+            <div id="imgp1">
+                <div id="myCarousel2" class="carousel slide" data-ride="carousel">
+                <!-- Indicador -->
+                <ul class="carousel-indicators">
               
-            </ul>
+                </ul>
         
             <!-- contenedor de slider -->
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <img src="img/key2.jpg" alt="1" style="width:100%;">
-                    <div class="carousel-caption d-none d-md-block" style="top:25%" id="titl3">
+                    <div class="carousel-caption d-block d-md-block" style="top:25%" id="titl3">
                         <h3 >Las herramientas para triunfar en el mundo digital al alcance de tus manos</h3>
     
                     </div>
