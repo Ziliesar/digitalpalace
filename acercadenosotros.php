434dfa0bb6aa2c595dc2ca358ed8cc62b6474e19
Espacio <heberzp3@gmail.com>
2020-06-27 20:26:59 -0600

    Responsive acercade

--- acercadenosotros.php
+++ acercadenosotros.php
@@ -156,7 +156,160 @@
 </div>
     <script src="js/main.js"></script>
 </div>
+
+<div id="contobj3">
+
+        <div style="color:#143f52">
+        <h1 class="QNS">¿Quiénes Somos?</h1>
+            <p style="padding-left:10%; padding-right:10%; padding-bottom:5%; text-align: justify">
+                Una empresa dedicada al desarrollo web, creación de software y mantenimiento, comprometida con la calidad de nuestros servicios en el entorno de la programación calificada, con las mejores opciones en proyectos para la obtención de aplicaciones, páginas web, softwares y más productos a medida de las necesidades de nuestros clientes, siguiendo en todos los casos una metodología de desarrollo, aplicando los procedimientos y controles del sistema de gestión de calidad, donde el producto del cliente tenga mayor prontitud en la obtención de su proyecto, un mantenimiento eficaz, con el mejor diseño y a un precio económico, asegurando el mayor rendimiento y particularidad de dicho producto, debido a que la empresa es constante en la evolución, garantía y calidad de los elementos en que se basan nuestros servicios para la creación del proyecto de desarrollo de software, con un diseño único y adaptado para la persona, institución o empresa que lo requiera.
+            </p>
+        </div>
+
+        
+        <div class="misi" style="padding:3%;;margin-top:0%; background-color:#143f52; color:white; text-align:center;">
+            <div>
+                <img src="acercadenosotros/mision.PNG" width="80%" height="40%" style="padding-bottom: 3%;">
+                <br>
+                <p class="font-center" style="text-align:justicy">Brindar excelencia y calidad en cada uno de nuestros servicios prestados, proporcionando las mejores herramientas tecnológicas a medida de las necesidades de nuestros clientes, con el objetivo de incrementar...</p>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalMision">
+                    Leer mas
+                </button>
+                
+                <div class="modal fade" id="modalMision" tabindex="-1" role="dialog" aria-labelledby="modalMision" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="modalMision" style="color: black;">Mision</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <p class="font-center" style="color: black;">
+                            Brindar excelo de nuestros servicios prestados, proporcionando las mejores herramientas tecnológicas a medida de las necesidades de nuestros clientes, con el objetivo de incrementar su productividad y competitividad en el mundo virtual. Para ello implementamos soluciones prácticas  y creativas, fundamentadas en el avance del desarrollo web, como empresa innovadora buscamos el máximo beneficio para nuestros clientes a través de la calidad de nuestros servicios y productos, creemos en el liderazgo empresarial responsable y en el desarrollo de un crecimiento rentable en conjunto con la sostenibilidad virtualmente progresiva.
+                            </p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            
+                        </div>
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="visi" style="padding:3%;;margin-top:0%; background-color:white; color:#143f52; text-align:center;">
+            <img src="acercadenosotros/vision.PNG" width="80%" style="padding-bottom: 3%;">
+            <br>
+            <p class="font-center">Estar altamente comprometidos con los trabajos realizados para nuestros clientes de manera calificada y eficaz, para convertirnos en su empresa de confianza, logrando identificarnos como una empresa ejemplar...</p>
+
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalvision">
+                    Leer mas
+                </button>
+                
+                <div class="modal fade" id="modalvision" tabindex="-1" role="dialog" aria-labelledby="modalvision" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="modalvision" style="color: black;">Vision</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <p class="font-center" style="color: black;">
+                            Estar altamente comprometidos con los trabajos realizados para nuestros clientes de manera calificada y eficaz, para convertirnos en su empresa de confianza, logrando identificarnos como una empresa ejemplar, que se involucra directamente con el cambio de la tecnología y la sociedad, dando a conocer las posibilidades de acceso a los estándares y herramientas tecnológicas, laborando de forma ética y garantizada. Nos comprometemos a que los productos y servicios sean seguros y estén disponibles para quién lo necesite, la meta que impulsa nuestra empresa, determina nuestro desempeño y propósito en el mundo exterior unificándolo con el mundo virtual de una forma completamente moderna.
+                            </p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            
+                        </div>
+                        
+                        </div>
+                    </div>
+                </div>
+        </div>
     
+
+
+
+
+
+
+    <div class="poli" style="padding:3%;;margin-top:0%; background-color:#143f52; color:white; text-align:center;">
+            <div>
+                <img src="acercadenosotros/politica.PNG" width="80%" height="40%" style="padding-bottom: 3%;">
+                <br>
+                <p class="font-center">Basada en nuestra firme responsabilidad con los clientes, nos encargamos de satisfacer plenamente sus requerimientos y expectativas...</p>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#PoliticaC">
+                    Leer mas
+                </button>
+                
+                <div class="modal fade" id="PoliticaC" tabindex="-1" role="dialog" aria-labelledby="PoliticaC" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="PoliticaC" style="color: black;">Politica de Seguridad</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <p class="font-center" style="color: black;">
+                                Basada en nuestra firme responsabilidad con los clientes, nos encargamos de satisfacer plenamente sus requerimientos y expectativas, por lo que impulsamos una cultura de calidad empresarial establecida en los principios de transparencia, liderazgo y desarrollo de nuestros servicios, comprometiéndonos a la mejora constante y la seguridad en cada uno de nuestros productos brindados.
+                            </p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            
+                        </div>
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="carac" style="padding:3%;;margin-top:0%; background-color:white; color:#143f52; text-align:center;">
+            <img src="acercadenosotros/caracteristicas.png" width="80%"  style="padding-bottom: 3%;">
+            <br>
+            <p class="font-center">Integridad: Reflejamos la honestidad y coherencia en todas nuestras acciones como empresa...</p>
+            
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#CaracterísticasV">
+                    Leer mas
+                </button>
+
+                <div class="modal fade" id="CaracterísticasV" tabindex="-1" role="dialog" aria-labelledby="CaracterísticasV" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="CaracterísticasV" style="color: black;">Características y valores</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            
+                                <p class="font-center" style="color: black;"><b>Integridad:</b> Reflejamos la honestidad y coherencia en todas nuestras acciones como empresa.</p>
+                                <p class="font-center" style="color: black;"><b>Responsabilidad:</b> Asumimos compromisos y cumplimos.</p>
+                                <p class="font-center" style="color: black;"><b>Calidad:</b> Presentamos un esfuerzo constante por mejorar cada vez más nuestros servicios como empresa, orientados al empeño de realizar bien cada acción desde la primera vez.</p>
+                                <p class="font-center" style="color: black;"><b>Respeto:</b> Brindamos respeto y cordialidad con cada uno de los clientes.</p>
+                                <p class="font-center" style="color: black;"><b>Vocación de Servicio:</b> Nos adaptamos, diseñamos e ideamos oportunamente, según las expectativas de nuestros clientes cada servicio y producto requerido.</p>
+                            
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            
+                        </div>
+                        
+                        </div>
+                    </div>
+                </div>
+        </div>
+    
+    </div>
 </body>
 
 </html>
